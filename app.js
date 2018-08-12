dba77ddee3c12d2fe1d2874ec56bf7f0462f705e
Michelle Tran Luu <miss_amtl@live.com>
2018-08-12 15:58:43 +0200

    gs

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const app=express();
     Here we are configuring our SMTP Server details.
     STMP is mail server which is responsible for sending and recieving email.
 */
-var smtpTransport = nodemailer.createTransport({
-    service: "IMAP",
-    host: "cpsrv07.misshosting.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: "info@jackistyle",
-        pass: process.env.email_password
-    }
-});
+const transporter = nodemailer.createTransport({
+  host: 'cpsrv07.misshosting.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'info@jackistyle.se',
+    pass: process.env.email_password
+  }
+})
 /*------------------SMTP Over-----------------------------*/
 
 
@@ -23,20 +22,16 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 
 app.post('/register',function(req,res){
-    var mailOptions={
-        to : req.query.to,
-    }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-     if(error){
-            console.log(error);
-        res.end("error");
-     }else{
-            console.log("Message sent: " + response.message);
-        res.end("sent");
-         }
-       });
-});
+  const email = req.query.email
+
+  transporter.sendMail({
+    from: 'info@jackistyle.se',
+    to: 'info@jackistyle.se',
+    subject: 'New sign up!',
+    text: email,
+    html: email
+  }).then(info => res.send(info)).catch(e => res.send(e))
+})
 
 app.listen(process.env.PORT || 3000,function(){
 console.log("Express Started on Port 3000");
