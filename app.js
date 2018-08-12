6b1399614ece01bdd9f666c0995fef4e1928a205
Michelle Tran Luu <miss_amtl@live.com>
2018-08-12 17:53:13 +0200

    as

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ const transporter = nodemailer.createTransport({
   }
 })
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
