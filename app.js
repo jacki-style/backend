9f0aa26849ad381eafd47d5bf97a3a38591271f8
Michelle Tran Luu <miss_amtl@live.com>
2018-08-12 14:33:48 +0200

    canged

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const app=express();
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
-app.get('/register',function(req,res){
-  console.log("SENT")
-});
+app.get('/register', (req, res) => res.send('Sent'))
 
 app.listen(process.env.PORT || 3000,function(){
 console.log("Express Started on Port 3000");
