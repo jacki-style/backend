ee4e6f81bb633d885d27c841ea5b4494aa5d6221
Michelle Tran Luu <miss_amtl@live.com>
2018-08-12 14:21:00 +0200

    added procfile and view

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express=require('express');
 const nodemailer = require("nodemailer");
 const app=express();
 
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.get('/register',function(req,res){
+  console.log("SENT")
+});
+
 app.listen(process.env.PORT || 3000,function(){
 console.log("Express Started on Port 3000");
 });
